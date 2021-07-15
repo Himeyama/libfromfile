fbf31fb9443804bd53ceabd8795f09fb5e12fb2e
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2021-07-15 20:26:59 +0900

    Update README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# LibFromfile
+
+## install
+```sh
+curl -s https://raw.githubusercontent.com/Himeyama/libfromfile/main/install.sh | bash
+```
--- local-install.sh
+++ local-install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 https://github.com/himeyama/libfromfile.git /tmp/libfromfile
-cd /tmp/libfromfile
 make -j
 sudo mkdir -p /usr/local/include
 sudo cp -v fromfile.h /usr/local/include/
